91eeb836907c23a6a9f5aa30b5d3782c6e5b4a18
DeadDingo <thedeaddingo@gmail.com>
2013-11-17 00:01:15 -0800

    Project Update

--- cscd240/Lab8/lab8.h
+++ cscd240/Lab8/lab8.h
@@ -2,16 +2,25 @@
 #define cscd240_lab8_h
 
 #define null NULL
+
 #include <string.h>
 
 #endif
 
 
-/*Displays Usage*/
+
+/*Methods that interface directly with the user
+* */
 void usage( void );
 
 /*Displays file format error*/
 void fileFormatError( void );
+
+//Displays Menu
+int menu( void );
+
+//================================
+
 
 /*Opens file for reading and returns file pointer
 * */
@@ -24,3 +33,10 @@ int readMonth(FILE *fin);
 /*Populates the Array
 * */
 int *fillArray(int month, FILE *fin);
+
+
+//=================================
+
+void displayMaxTemp(int *temps);
+
+void displayMinTemp(int *temps);
