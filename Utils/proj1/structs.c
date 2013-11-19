dc8b8d28f7c91de481f0b43acfe4555c833a02f8
DeadDingo <thedeaddingo@gmail.com>
2013-11-19 09:05:15 -0800

    Update

---
+++ Utils/proj1/structs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct database {
+
+  int id;
+  float salary;
+  char *name;
+
+};
+
+int main( int argc, char *argv[] ) {
+
+  int i;
+  
+  //
+  for(i = 0; i < argc; i++) {
+
+  }
+
+
+}
---
+++ cscd240/lab9/cscd240Lab9.c
@@ -0,0 +1,36 @@
+#include "lab9.h"
+
+int main()
+{
+    FILE * fin = NULL;
+    int total, choice;
+    Address array[10]; // guaranteed 1 to 10 inclusive
+
+    fin = openFile();
+
+    total = fillArray(array, fin);
+
+    fclose(fin);
+    fin = NULL;
+
+    do
+    {
+        switch(choice = menu())
+        {
+			case 1:	printStreetSortedArray(array, total);
+					break;
+
+			case 2: printCitySortedArray(array, total);
+					break;
+
+			case 3: printStateCitySortedArray(total, array);
+					break;
+
+			case 4:	printZipSortedArray(total, array);
+		}
+
+	}while(choice != 5);
+
+    return 0;
+
+}// end main
