55785b182b27607bddc8339936b31a285bd75ae3
DeadDingo <thedeaddingo@gmail.com>
2013-11-23 14:12:33 -0800

    lab update

---
+++ cscd240/Lab10/Harshmanj/lab10.h
@@ -0,0 +1,39 @@
+//lab10.h
+//cscd240
+//
+//Josh Harshman
+//
+
+#ifndef lab10_h
+#define lab10_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Address {
+
+  char *street;
+  char *city;
+  char *state;
+  int zip;
+
+}Address;
+
+FILE *openFile(void);
+
+Address *fillArray(int *total, FILE *fin);
+
+int menu(void);
+
+void printStreetSortedArray(Address *array, int total);
+
+void printCitySortedArray(Address *array, int total);
+
+void printStateCitySortedArray(int total, Address *array);
+
+void printZipSortedArray(int total, Address *array);
+
+void cleanUp(Address *array, int total);
+
+#endif
