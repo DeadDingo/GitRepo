5b11792bb7dd1f6cf6d8dbfe062b2b66063f4de2
DeadDingo <thedeaddingo@gmail.com>
2013-11-24 23:57:56 -0800

    update

--- cscd240/Hw4/wordUtil.c
+++ cscd240/Hw4/wordUtil.c
@@ -14,3 +14,23 @@
 #include "wordUtil.h"
 
 //define all funcions in wordUtil.h here
+
+listnode *extract( const char *line, listnode *oldHead ) {
+
+  int i;
+  int size = strlen(line);
+  char buffer[MAXWORDLEN];
+
+  while(line[i] != '\0') {
+
+    if(isLetter( line[i] ) {
+	strcpy(buffer[i], line[i]); //copy into temporary word buffer
+    }
+
+  }
+
+    //insert word into node
+
+    //return new pointer to linked list
+
+}
