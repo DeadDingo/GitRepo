b44d7f01c2c576f0ca0963f542246c5f92f8bdac
DeadDingo <thedeaddingo@gmail.com>
2013-12-04 16:31:19 -0800

    implemented qsort for street names

--- cscd240/Lab11/lab11.h
+++ cscd240/Lab11/lab11.h
@@ -34,15 +34,20 @@ void printStateCitySortedArray(int total, Address *array);
 
 void printZipSortedArray(int total, Address *array);
 
-//Replace structBubbleSort() with compare method for use with qsort()
 //compare by Street function
+int streetCompare(const void *a, const void *b);
 //compare by city function
+int cityCompare(const void *a, const void *b);
 //compare by state function
+int stateCompare(const void *a, const void *b);
 //compare by zip function
+int zipCompare(const void *a, const void *b);
+
 void structBubbleSort(Address *array, int index);
 
 void printAll(Address *array, int total);
 
 void cleanUp(Address *array, int total);
+
 
 #endif
