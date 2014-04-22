c47587fe56fb99879cf09d83e0d0ece815be9dfe
deaddingo <thedeaddingo@gmail.com>
2014-04-22 16:41:15 -0700

    threads7.c update

--- Utils/Testing/threads7.c
+++ Utils/Testing/threads7.c
@@ -81,21 +81,29 @@ int main(void) {
   int i, rc;
   long t1=1, t2=2, t3=3;
   pthread_t threads[3];
-  pthread_attr attr;
+  pthread_attr_t attr;
   
   //init mutex and condition vars
   pthread_mutex_init(&count_mutex, NULL);
   pthread_cond_init(&count_threshold_cv, NULL); //note null attribute (previous line as well)
 
   pthread_attr_init(&attr);
-  pthread_setdetatchstate(&attr, PTHREAD_CREATE_JOINABLE);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
   pthread_create(&threads[0], &attr, watch_count, (void *)t1);
   pthread_create(&threads[1], &attr, inc_count, (void *)t2);
   pthread_create(&threads[2], &attr, inc_count, (void *)t3);
 
 
-  
+  for(i = 0; i < NUM_THREADS; i++) {
+    pthread_join(threads[i], NULL);
+  }
 
+  printf("Main(): Waited on %d threads done.\n", NUM_THREADS);
+  
+  //clean up before exiting
+  pthread_attr_destroy(&attr);
+  pthread_mutex_destroy(&count_mutex);
+  pthread_cond_destroy(&count_threshold_cv);
 
   pthread_exit(NULL);
 
