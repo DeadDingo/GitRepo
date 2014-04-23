8c1b0dbc6e7ac156e184310d7fd38c31bef94fbc
deaddingo <thedeaddingo@gmail.com>
2014-04-23 14:46:06 -0700

    update

---
+++ ClientServer/Server/headers/hander.h
@@ -0,0 +1,33 @@
+/**
+ * handler.h
+ * 
+ * Written By Josh Harshman
+ * April 23, 2014
+ * 
+ * */
+
+#ifndef HANDLER_H
+#define HANDLER_H
+
+//----[include]----
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "colors.h"
+
+//----[end include]----
+
+
+
+
+
+#endif
